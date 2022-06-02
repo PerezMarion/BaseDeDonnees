29076a14e2b80baa11386cf3f88ad0a0d12e1561
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-02 17:12:56 +0200

    version td

---
+++ telephones.sql
@@ -0,0 +1,46 @@
+-- 1. Créer une base de données appelée sales
+
+CREATE DATABASE sales;
+
+USE sales;
+
+-- 2. Créer une table appelée telephones avec les colonnes suivantes :
+-- name : chaîne de caractères qui représente le modèle du téléphone
+-- manufacturer : chaîne de caractères qui représente le fabricant
+-- price : un nombre qui représente le prix UHT
+-- units_sold : nombre qui représentent la quantité vendue
+
+CREATE TABLE telephones(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    manufacturer VARCHAR(100) NOT NULL,
+    price INTEGER NOT NULL,
+    units_sold INTEGER);
+
+-- 3. Insérer les informations
+
+INSERT INTO telephones (name, manufacturer, price, units_sold) VALUES
+("N1280","Nokia",199,1925),
+("Iphone 4","Apple",399,9436),
+("Galaxy S 5","Samsung",299,2359),
+("S5620 Monte","Samsung",250,2385),
+("N8","Nokia",150,7543),
+("Droid","Motorola",150,8395),
+("Iphone 13 Pro Max","Apple",1300,12849),
+("Galaxy Note 20","Samsung",1100,10353);
+
+-- 4. Ecrire une requête pour liste tous les modèles de téléphones
+
+SELECT name FROM telephones;
+
+-- 5. Ecrire une requête pour lister tous les telephone(name, manufacturer) de tous les téléphones dont le prix est supérieur à 200€
+
+SELECT name, manufacturer FROM telephones where price>200;
+
+-- 6. Ecrire une requête pour lister tous les telephone(name, manufacturer) de tous les téléphone dont le prix est varie entre 150 et 200€
+
+SELECT name, manufacturer FROM telephones where price>=150 AND price <=200;
+
+-- 7. Lister tous les téléphones de marque Samsung et Apple
+
+SELECT * FROM telephones WHERE manufacturer='Apple' OR manufacturer='Samsung';
