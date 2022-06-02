19cb4c0aeac6d6e79a1491af9fecc9c043bc6a03
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-02 14:59:28 +0200

    insertion/lecture/modification de données

--- clients.sql
+++ clients.sql
@@ -38,12 +38,32 @@ telephone VARCHAR(20) NOT NULL);
 
 DESC clients
 
--- 8. Ajout d'une colonne dans la table client
+-- 8. Ajout d'une colonne dans la table clients
 
 ALTER TABLE clients ADD COLUMN postnom VARCHAR(50);
 -- Si ajout après une colonne en particulier
-ALTER TABLE clients ADD COLUMN postnom VACHAR(50) AFTER prenom;
+ALTER TABLE clients ADD COLUMN age INTEGER(2) AFTER postnom;
 
--- 9. Modification d'une colonne dans la table client
+-- 9. Modification d'une colonne dans la table clients
 
 ALTER TABLE clients MODIFY COLUMN email NOT NULL;
+
+-- 10. Insertion de données dans la table clients
+
+INSERT INTO clients(nom, prenom, email, telephone)
+values("perez","marion","marion.oceane.perez@gmail.com","0786842676"); 
+-- Si ajout multiple
+INSERT INTO clients(nom, prenom, email, telephone)
+values("dumortier","steven","stevendumortier59@gmail.com","0747474747"),
+("raulin","ludovic","lraulin843@gmail.com","0621461941");
+
+-- 11. Lire des données dans la table clients
+
+SELECT * FROM clients
+ou
+SELECT email FROM clients
+
+-- 12. Modification de données dans la table clients
+
+UPDATE clients SET age=25 WHERE id=1
+
