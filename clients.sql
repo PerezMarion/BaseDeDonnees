cfc0d9950a185868c3420ab2f18b2d5f58b76d3b
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-02 11:46:18 +0200

    colonne id

--- clients.sql
+++ clients.sql
@@ -10,8 +10,20 @@
 
 CREATE DATABASE poe_ventes;
 
--- 5. Utiliser/sélectionner la base de données
+-- 4. Utiliser/sélectionner la base de données
 
 USE poe_ventes;
 
--- 4. Création de la table clients
+-- 5. Lister les tables de la base de données
+
+SHOW TABLES;
+
+-- 6. Création de la table clients
+-- INTEGER : type "entier"
+-- NOT NULL : cette colonne doit toujours contenir une information
+-- AUTO_INCREMENT : fournir une valeur en incrémentant automatiquement les clés
+-- PRIMARY KEY : marqueuer la colonne comme étant la clé primaire
+CREATE TABLE clients(
+id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
+
+);
