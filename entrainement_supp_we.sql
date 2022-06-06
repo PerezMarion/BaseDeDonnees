e3cb4aa384fd59c26e608fd1a3822e7bcec8a032
Christian Lisangola <christian.lisangola@gmail.com>
2022-06-06 17:24:29 +0200

    fin entrainement

--- entrainement_supp_we.sql
+++ entrainement_supp_we.sql
@@ -85,3 +85,39 @@ INSERT INTO Notes_Francais(f_id,f_ds1,f_ds2,f_expose,f_ds3) VALUES
 (101,12,11,12,10);
 
 SELECT * FROM Notes_Francais;
+
+-- 4. Afficher le nom et prénom de tous les élèves ayant eu un avis défavorable en mathématiques
+
+SELECT Avis_classe_sup.Nom AS Nom, Avis_classe_sup.Prenom AS Prenom 
+FROM Avis_classe_sup JOIN Notes_Mathematiques ON Avis_classe_sup.id=Notes_Mathematiques.m_id
+WHERE Avis_classe_sup.Mathematiques="Defavorable";
+
+-- 5. Afficher le nom et prénom de tous les élèves n'ayant eu que des notes supérieures à 10 en devoir surveillé de francais 
+
+SELECT Avis_classe_sup.Nom AS Nom, Avis_classe_sup.Prenom AS Prenom
+FROM Avis_classe_sup JOIN Notes_Francais ON Avis_classe_sup.id=Notes_Francais.f_id
+WHERE Notes_Francais.f_ds1>10 AND Notes_Francais.f_ds2>10 AND Notes_Francais.f_ds3>10;
+
+-- 6. Créer une vue avec la moyenne de classe obtenue pour chaque devoir surveillé de mathématiques
+
+CREATE VIEW v_moyennes_mathematiques AS SELECT AVG(m_ds1) AS moyenne_m_ds1, AVG(m_ds2) AS moyenne_m_ds2, AVG(m_ds3) AS moyenne_m_ds3 
+FROM Notes_Mathematiques;
+
+SELECT * FROM v_moyennes_mathematiques
+
+-- 7. Afficher le nom, le prénom et la note obtenue par l'élève ayant la meilleure note à l'exposé de francais
+
+SELECT Avis_classe_sup.Nom AS Nom, Avis_classe_sup.Prenom AS Prenom, MAX(Notes_Francais.f_expose) AS Note_maximale
+FROM Avis_classe_sup JOIN Notes_Francais ON Avis_classe_sup.id=Notes_Francais.f_id;
+
+-- 8. Afficher le nombre d'élèves ayant eu 15 ou plus aux deux devoirs maisons de mathematiques
+
+SELECT COUNT(*) FROM Notes_Mathematiques WHERE m_dm1>=15 AND m_dm2>=15;
+
+-- 9. Classer par ordre alphabétique le nom des élèves
+
+SELECT id, Nom, Prenom FROM Avis_classe_sup ORDER By Nom ASC;
+
+-- 10. Calculer la moyenne de français de Camille Maillet
+
+SELECT ((f_ds1+f_ds2+f_ds3+f_expose)/4) AS moyenne_francais FROM Notes_Francais WHERE f_id=101;
